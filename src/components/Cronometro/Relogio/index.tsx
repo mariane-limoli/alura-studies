9b390247aa5ada77a5520b0ae51edcc09f62a08a
Mariane Limoli <marianelimoli@gmail.com>
2023-11-29 13:56:32 -0300

    add tempo

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,21 @@
 import '../Relogio/style.scss';
 
-export function Relogio() {
+interface Props {
+    tempo: number | undefined;
+ }
+
+export function Relogio({ tempo = 0 }: Props) {
+    const minutos = Math.floor(tempo / 60);
+    const segundos = tempo % 60;
+    const [minutosDezena, minutoUnidade] = String(minutos).padStart(2, '0');
+    const [segundosDezena, segundoUnidade] = String(segundos).padStart(2, '0');
     return (
         <>
-            <span className='relogioNumero'>0</span>
-            <span className='relogioNumero'>0</span>
+            <span className='relogioNumero'>{minutosDezena}</span>
+            <span className='relogioNumero'>{minutoUnidade}</span>
             <span className='relogioDivisao'>:</span>
-            <span className='relogioNumero'>0</span>
-            <span className='relogioNumero'>0</span>
+            <span className='relogioNumero'>{segundosDezena}</span>
+            <span className='relogioNumero'>{segundoUnidade}</span>
         </>
         
     )
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,10 +1,12 @@
+import { useEffect, useState } from "react";
+
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
 import Botao from "../Botao";
 import { Relogio } from "./Relogio";
-import { tempoParaSegundos } from "../../common/utils/time";
 
 import '../Cronometro/style.scss';
-import { ITarefa } from "../../types/tarefa";
-import { useEffect, useState } from "react";
+
 interface Props {
     selecionado: ITarefa | undefined;
 }
@@ -18,9 +20,8 @@ export function Cronometro({ selecionado }:Props) {
     return (
         <div className="cronometro">
             <p className="titulo">Escolha um card e inicie o cronômetro</p>
-            Tempo: {tempo}
             <div className="relogioWrapper">
-                <Relogio />
+                <Relogio tempo ={tempo}/>
             </div>
             <Botao texto="Começar" />
         </div>
